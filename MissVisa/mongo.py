b2adddde84750a68c8979954c1920520c2fc8a07
CALLMEVP <82378057+callmevp@users.noreply.github.com>
2021-08-28 16:18:15 +0530

    visa

--- MissVisa/mongo.py
+++ MissVisa/mongo.py
@@ -1,24 +1,24 @@
-#HuntingBots
+#Visabots
 import asyncio
 import sys
 
 from motor import motor_asyncio
-from AsunaRobot import MONGO_DB_URI 
+from MissVisa import MONGO_DB_URI 
 from pymongo import MongoClient
 from pymongo.errors import ServerSelectionTimeoutError
-from AsunaRobot.conf import get_int_key, get_str_key
+from MissVisa.conf import get_int_key, get_str_key
 
 
 MONGO_PORT = get_int_key("27017")
 MONGO_DB_URI = get_str_key("MONGO_DB_URI")
-MONGO_DB = "AsunaRobot"
+MONGO_DB = "MissVisa"
 
 
 client = MongoClient()
 client = MongoClient(MONGO_DB_URI, MONGO_PORT)[MONGO_DB]
 motor = motor_asyncio.AsyncIOMotorClient(MONGO_DB_URI, MONGO_PORT)
 db = motor[MONGO_DB]
-db = client["AsunaRobot"]
+db = client["MissVisa"]
 try:
     asyncio.get_event_loop().run_until_complete(motor.server_info())
 except ServerSelectionTimeoutError:
